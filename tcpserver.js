f50453095a8c1e4c825913bdf2bdcb3b1a32d958
alteruca <alteruca@hotmail.com>
2017-02-02 16:01:33 +0100

    Update tcpserver.js

--- tcpserver.js
+++ tcpserver.js
@@ -1,1 +1,40 @@
+// Group 2 : Embedded Systems of Computer Science, UDE, Germany
+// AMAN BATRA , 3022386
+// Alvaro Teruel Cañones 
+// Montse Rodríguez Zamorano
+
+// Requires the following additional packages. From the directory containing tcpserver.js, run:
+// npm install particle-api-js yargs node-persist
+// 
+
+// Run:node tcpserver.js --login <user> <password> 
+// Atlease Once to generate the access token used to access the Particle cloud.
+
+var path = require('path');
+
+
+var setup = require('./library/setup.js'); 
+setup.settingsDir = path.join(__dirname, 'settings');
+
+ 
+var cloud = require('./library/cloud.js');
+setup.addModule(cloud);
+
+
+var devices = require('./library/devices.js');
+setup.addModule(devices);
+cloud.addEventHandler('devices', devices);
+
+
+var server = require('./library/server.js');
+server.serverPort = 8070;
+setup.addModule(server);
+devices.server = server;
+
+
+setup.init();
+
+
+
+
 
